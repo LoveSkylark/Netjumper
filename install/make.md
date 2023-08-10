60ca530b5e9d2e1f78b48f98211cb94252719275
LoveSkylark <s.stefan.sig@gmail.com>
2023-08-10 13:16:28 +0000

    cleanup

--- install/make.md
+++ install/make.md
@@ -12,6 +12,7 @@ sudo apt install netcat
 # pyhton3 native in ubuntu 22.04
 sudo apt install python-is-python3 
 sudo apt install pip
+sudo apt install python3-dotenv
 
 sudo pip install certifi
 sudo pip install python-hosts
--- scripts/lnms_firmware.py
+++ scripts/lnms_firmware.py
@@ -2,20 +2,15 @@
 
 import os
 import sys
+import argparse
 from Libs.LibreNMSAPIClient.LibreNMSAPIClient import LibreNMSAPIClient
-
-def get_argument(index):
-    if len(sys.argv) > index:
-        return sys.argv[index]
-    else:
-        return None
 
 def group_devices_by_hardware_version(_device):
     # Group devices by hardware type, then by version, 
     # and sort IP addresses within each group
     result = {}
     for device in _device:
-        if device['hardware'] != None:
+        if device['hardware'] is not None:
             hardware = device['hardware'].replace(" ", "_").replace("/", "_")
             version = device['version']
             ip = device['hostname']
@@ -29,54 +24,64 @@ def group_devices_by_hardware_version(_device):
             result[hardware][version].append(ip)
     return result
 
-def save_devices_to_files(_list):
+def save_devices_to_files(_list, output_dir):
     for hardware, version_dict in sorted(_list.items()):
         filename = hardware.replace(" ", "_").replace("/", "_")
-        f=open(output_dir + "list_firmware/" + filename + ".txt","w")
-        for version, ip_list in sorted(version_dict.items()):
-            f.write(f"> {version}\n")
-            for ip in sorted(ip_list):
-                f.write(f"  {ip}\n")
-        f.close()
+        with open(output_dir + "list_firmware/" + filename + ".txt", "w") as f:
+            for version, ip_list in sorted(version_dict.items()):
+                f.write(f"> {version}\n")
+                for ip in sorted(ip_list):
+                    f.write(f"  {ip}\n")
 
-def print_device_list(_list):
+def print_device_list(_list, hardware_filter, version_filter):
     for hardware, version_dict in sorted(_list.items()):
-        if not get_argument(1) or get_argument(1) == hardware:
+        if not hardware_filter or hardware_filter == hardware:
             print(hardware)
-            if get_argument(1):
+            if hardware_filter:
                 for version, ip_list in sorted(version_dict.items(), reverse=True):
-                    if not get_argument(2):
+                    if not version_filter:
                         print("  ", version)
                         for ip in sorted(ip_list):
-                            print("    ",ip)
+                            print("    ", ip)
                     else:
                         for ip in sorted(ip_list):
-                            if version <= get_argument(2):
+                            version_str = " ".join(version_filter)
+                            if str(version_str) <= str(version):
                                 print(ip)
 
-def print_help():
-    if not get_argument(2):
+
+def print_help(hardware_filter, version_filter):
+    if not version_filter:
         print()
         print("Add model name and serial number to narrow list")
         print("Examples:")
-        [print("     ./lnms_firmware WS-C4500X-32") if not get_argument(1) else None]
+        [print("     ./lnms_firmware WS-C4500X-32") if not hardware_filter else None]
         print("     ./lnms_firmware WS-C4500X-32 03.11")
 
-# API connect to LibreNMS & get device list
-lnms = LibreNMSAPIClient()
-lnms_devices = lnms.list_devices()
+def main():
+    parser = argparse.ArgumentParser(description="Script to download LibreNMS configuration")
+    parser.add_argument("hardware_filter", nargs="?", help="Hardware model name to filter on")
+    parser.add_argument("version_filter", nargs=argparse.REMAINDER, help="Version to filter on")
+    args = parser.parse_args()
 
-# File location
-output_dir = os.environ['info_dir']
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-if not os.path.exists(f"{output_dir}/list_firmware"):
-    os.makedirs(f"{output_dir}/list_firmware")
+    # API connect to LibreNMS & get device list
+    lnms = LibreNMSAPIClient()
+    lnms_devices = lnms.list_devices()
 
-lnms_list = group_devices_by_hardware_version(lnms_devices)
+    # File location
+    output_dir = os.environ['info_dir']
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    if not os.path.exists(f"{output_dir}/list_firmware"):
+        os.makedirs(f"{output_dir}/list_firmware")
 
-save_devices_to_files(lnms_list)
+    lnms_list = group_devices_by_hardware_version(lnms_devices)
 
-print_device_list(lnms_list)
+    save_devices_to_files(lnms_list, output_dir)
 
-print_help()
+    print_device_list(lnms_list, args.hardware_filter, args.version_filter)
+
+    print_help(args.hardware_filter, args.version_filter)
+
+if __name__ == "__main__":
+    main()
